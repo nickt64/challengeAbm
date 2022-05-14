6000c1d9a2965e389856a472530924ba3ba8cfa2
Nicolas Tomeo <87679478+nickt64@users.noreply.github.com>
2022-05-13 23:22:08 -0300

    validaciones

--- Shared/Dtos/NuevoUsuarioDto.cs
+++ Shared/Dtos/NuevoUsuarioDto.cs
@@ -13,17 +13,20 @@ namespace Shared.Dtos
         public int Id { get; set; }
 
         [Required]
+        [RegularExpression(@"^([a-zA-ZñÑ\s]{2,50})$", ErrorMessage = "solo letras minimo 2 caracteres")]
         public string Nombre { get; set; }
 
         [Required]
+        [RegularExpression(@"^([a-zA-ZñÑ\s]{2,50})$", ErrorMessage = "solo letras minimo 2 caracteres")]
         public string Apellido { get; set; }
 
         [Required]
         public string UserName { get; set; }
 
-        [Required]
+        [Required,EmailAddress]
         public string Email { get; set; }
 
+        [RegularExpression(@"^([0-9]{5,15})$", ErrorMessage = "Solo numeros minimo 5 numeros.")]
         public string Telefono { get; set; }
 
         [DisplayName("Estado")]
